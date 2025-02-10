83a3b6f2a19cf23068be5e023187ea13b92e05b4
Muhammad Saad <72243425+saadsafda@users.noreply.github.com>
2025-02-10 19:47:52 +0500

    update increment

--- shaigan_hr/shaigan_hr/overrides/override_employee_increment.py
+++ shaigan_hr/shaigan_hr/overrides/override_employee_increment.py
@@ -21,26 +21,13 @@ class OverrideEmployeeIncrement(EmployeeIncrement):
 
 	def on_submit(self):
 		salary_sturcture = self.get_structure_asignment()
-		tax_slab = frappe.get_last_doc("Income Tax Slab", filters={
-			"company": self.company,
-			"disabled": 0,
-			"docstatus": 1,
-			"currency": salary_sturcture.currency
-		})
-		salary_sturcture = frappe.get_doc({
-			"doctype": "Salary Structure Assignment",
-			"employee": self.employee,
-			"from_date": self.increment_date,
-			"salary_structure": salary_sturcture.salary_structure,
-			"company": self.company,
-			"payroll_payable_account": salary_sturcture.payroll_payable_account,
-			"currency": salary_sturcture.currency,
-			"income_tax_slab": tax_slab.name,
-			"base": self.revised_salary,
-			"docstatus": 1,
-			"employee_increment": self.name
-		})
-		salary_sturcture.insert(ignore_permissions=True)
+		new_salary_structure = frappe.copy_doc(salary_sturcture)
+		new_salary_structure.name = None
+		new_salary_structure.company = self.company
+		new_salary_structure.base = self.revised_salary
+		new_salary_structure.employee_increment = self.name
+		new_salary_structure.from_date = self.increment_date
+		new_salary_structure.insert(ignore_permissions=True)
 
 		arrears_setting = frappe.get_doc("Arrears Process Setting")
 		salary_slip = frappe.db.exists("Salary Slip", {
@@ -69,8 +56,6 @@ class OverrideEmployeeIncrement(EmployeeIncrement):
 
 			monthly_salary, old_days, new_days = calculate_salary_by_dates(self.current_salary, self.revised_salary, str(start_date), str(end_date), str(self.increment_date))
 
-			print(monthly_salary, old_days, new_days, "monthly_salary, old_days, new_days \n\n\n")
-			print(curr_basic, self.current_salary, self.revised_salary, str(start_date), str(end_date), str(self.increment_date), "params \n\n\n")
 
 			
 			salary_slip = self.get_salary_slip(self.employee, start_date, end_date_, new_days)
@@ -82,7 +67,6 @@ class OverrideEmployeeIncrement(EmployeeIncrement):
 			salary_slip.custom_monthly_salary = self.current_salary
 			# salary_slip.payment_days =  salary_slip.payment_days - new_days
 			salary_slip.calculate_net_pay()
-			print("start Date", start_date, "end date", end_date_, "salary_slip.earnings \n\n\n\n", salary_slip.custom_payment_day, salary_slip.payment_days, salary_slip.custom_base, " first slary base\n\n")
 
 
 			salary_slip1 = self.get_salary_slip(self.employee, self.increment_date, end_date, old_days)
@@ -94,7 +78,6 @@ class OverrideEmployeeIncrement(EmployeeIncrement):
 			salary_slip1.custom_monthly_salary = self.revised_salary
 			# salary_slip1.payment_days =  salary_slip1.payment_days - old_days
 			salary_slip1.calculate_net_pay()   
-			print("start Date", self.increment_date, "end date", end_date, "salary_slip.earnings \n\n\n\n", salary_slip1.custom_payment_day, salary_slip1.payment_days, salary_slip1.custom_base, " Second slary base\n\n")
 
 			from_date = add_days(end_date, 1)
 			end_date = frappe.call('hrms.payroll.doctype.payroll_entry.payroll_entry.get_end_date', 
@@ -136,7 +119,6 @@ class OverrideEmployeeIncrement(EmployeeIncrement):
 								c_salary.amount = arrears_basic
 
 								break
-			print(total_earning, "Total Earning \n\n\n\n")
 			total_deduction = 0
 			for c_salary in emp_arrears.e_a_deductions:
 				arrears_basic = 0
@@ -195,7 +177,6 @@ class OverrideEmployeeIncrement(EmployeeIncrement):
 				"amount": arears_amount,
 				"to": end_date["end_date"]
 			})
-			print(arears_amount, total_basic, curr_basic, sal_2_basic, sal_1_basic, "arears_amount \n\n\n\n")
 			a_p_earn_existing = []
 			a_p_deduct_existing = []
 
@@ -249,8 +230,8 @@ def calculate_salary_by_dates(old_salary, new_salary, start_date_str, end_date_s
     if new_salary_days > 30:
         new_salary_days = 30  # If new salary days exceed 30, adjust to 30
     old_salary_days = (increment_date - start_date).days if increment_date > start_date else 0
-    print(f"New Salary Days: {new_salary_days}")
-    print(f"Old Salary Days: {old_salary_days}")
+    # print(f"New Salary Days: {new_salary_days}")
+    # print(f"Old Salary Days: {old_salary_days}")
     if old_salary_days > 0 and new_salary_days == 30:
         new_salary_days = new_salary_days - old_salary_days
     # Adjust for the last day increment and ensure total days are 30
@@ -258,20 +239,17 @@ def calculate_salary_by_dates(old_salary, new_salary, start_date_str, end_date_s
         old_salary_days = 29  # 29 days for old salary
         new_salary_days = 1   # 1 day for new salary
     else:
-        print(f"Total Days: {total_days}")
         # new_salary_days = total_days - old_salary_days
         old_salary_days = total_days - new_salary_days
 
     # If total_days is more than 30, adjust 1 day to the new salary
     if total_days > 30:
-        print(f"Total Days old under: {old_salary_days} {(increment_date - start_date).days}")
         if new_salary_days > old_salary_days and (increment_date - start_date).days + new_salary_days > 30:
             old_salary_days = old_salary_days
             new_salary_days = 30 - old_salary_days
         old_salary_days = 30 - new_salary_days
         # new_salary_days = 30 - old_salary_days
     elif total_days < 30:
-        print("Total days should be 30 or more")
         new_salary_days += 1
 
     # Daily wages
